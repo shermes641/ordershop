3a2c89ab873019cd520ad336e3314db582ed26ea
shermes641 <stevehermes@gmail.com>
2019-05-19 13:17:13 -0600

    Make the crm service just like the others

--- crm_service/crm_service.py
+++ crm_service/crm_service.py
@@ -1,79 +1,118 @@
-import atexit
 import json
+from datetime import timedelta
+
 import requests
+from flask import Flask
+from flask import request, abort
+from timeloop import Timeloop
+
+from common.service_base import ServiceBase
+from common.utils import log_error, log_info, ChkClass
 from lib.event_store import EventStore
-from common.utils import log_error, log_info
 
 
-store = EventStore('crm')
+class CrmService(ServiceBase):
+    def __init__(self):
+        self.chk_class = ChkClass('crm')
+        self.store = EventStore(self.chk_class.name)
+        self.init_store(self.chk_class, self.store)
+
+    @staticmethod
+    def customer_created(item):
+        try:
+            msg_data = json.loads(item[1][0][1]['entity'])
+            msg = """Dear {}!
+    
+    Welcome to Ordershop.
+    
+    Cheers""".format(msg_data['name'])
+            log_info(msg)
+            requests.post('http://msg-service:5000/email', json={
+                "to": msg_data['email'],
+                "msg": msg
+            })
+        except Exception as e:
+            log_error(e)
+
+    @staticmethod
+    def customer_deleted(item):
+        try:
+            msg_data = json.loads(item[1][0][1]['entity'])
+            msg = """Dear {}!
+    
+    Good bye, hope to see you soon again at Ordershop.
+    
+    Cheers""".format(msg_data['name'])
+            log_info(msg)
+            requests.post('http://msg-service:5000/email', json={
+                "to": msg_data['email'],
+                "msg": msg
+            })
+        except Exception as e:
+            log_info('CRM DELETE EXCEPTION')
+            log_error(e)
+
+    def order_created(self, item):
+        try:
+            msg_data = json.loads(item[1][0][1]['entity'])
+            customer = self.store.find_one('customer', msg_data['customer_id'])
+            products = [self.store.find_one('product', product_id) for product_id in msg_data['product_ids']]
+            msg = """Dear {}!
+    
+    Thank you for buying following {} products from Ordershop:
+    {}
+    
+    Cheers""".format(customer['name'], len(products), ", ".join([product['name'] for product in products]))
+            log_info(msg)
+            requests.post('http://msg-service:5000/email', json={
+                "to": customer['email'],
+                "msg": msg
+            })
+        except Exception as e:
+            log_error(e)
+
+    def subscribe_to_domain_events(self):
+        self.store.subscribe('customer', 'created', self.customer_created)
+        self.store.subscribe('customer', 'deleted', self.customer_deleted)
+        self.store.subscribe('order', 'created', self.order_created)
+
+    def unsubscribe_from_domain_events(self):
+        self.store.unsubscribe('customer', 'created', self.customer_created)
+        self.store.unsubscribe('customer', 'deleted', self.customer_deleted)
+        self.store.unsubscribe('order', 'created', self.order_created)
 
 
-def customer_created(item):
-    try:
-        msg_data = json.loads(item[1][0][1]['entity'])
-        msg = """Dear {}!
+crm = CrmService()
 
-Welcome to Ordershop.
-
-Cheers""".format(msg_data['name'])
-        log_info(msg)
-        requests.post('http://msg-service:5000/email', json={
-            "to": msg_data['email'],
-            "msg": msg
-        })
-    except Exception as e:
-        log_error(e)
+tl = Timeloop()
 
 
-def customer_deleted(item):
-    try:
-        msg_data = json.loads(item[1][0][1]['entity'])
-        msg = """Dear {}!
-
-Good bye, hope to see you soon again at Ordershop.
-
-Cheers""".format(msg_data['name'])
-        log_info(msg)
-        requests.post('http://msg-service:5000/email', json={
-            "to": msg_data['email'],
-            "msg": msg
-        })
-    except Exception as e:
-        log_info('CRM DELETE EXCEPTION')
-        log_error(e)
+@tl.job(interval=timedelta(seconds=5))
+def rc():
+    crm.redis_chk(crm.chk_class, crm.store)
 
 
-def order_created(item):
-    try:
-        msg_data = json.loads(item[1][0][1]['entity'])
-        customer = store.find_one('customer', msg_data['customer_id'])
-        products = [store.find_one('product', product_id) for product_id in msg_data['product_ids']]
-        msg = """Dear {}!
+tl.start()
 
-Thank you for buying following {} products from Ordershop:
-{}
-
-Cheers""".format(customer['name'], len(products), ", ".join([product['name'] for product in products]))
-        log_info(msg)
-        requests.post('http://msg-service:5000/email', json={
-            "to": customer['email'],
-            "msg": msg
-        })
-    except Exception as e:
-        log_error(e)
+app = Flask(__name__)
 
 
-def subscribe_to_domain_events():
-    store.subscribe('customer', 'created', customer_created)
-    store.subscribe('customer', 'deleted', customer_deleted)
-    store.subscribe('order', 'created', order_created)
-
-
-def unsubscribe_from_domain_events():
-    store.unsubscribe('customer', 'created', customer_created)
-    store.unsubscribe('customer', 'deleted', customer_deleted)
-    store.unsubscribe('order', 'created', order_created)
-
-
-subscribe_to_domain_events()
-atexit.register(unsubscribe_from_domain_events)
+@app.route('/health', methods=['GET'])
+@app.route('/ready', methods=['GET'])
+@app.route('/restart', methods=['GET'])
+def get():
+    if 'health' in request.path:
+        if crm.chk_class.restart:
+            abort(500)
+        else:
+            return json.dumps(True)
+    elif 'ready' in request.path:
+        if crm.chk_resdis_store_bad(crm.chk_class, crm.store):
+            abort(503)
+        else:
+            return json.dumps(True)
+    elif 'restart' in request.path:
+        crm.chk_class.restart = True
+        return json.dumps(True)
+    else:
+        return json.dumps(False)
